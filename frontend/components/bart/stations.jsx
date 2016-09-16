a78764179f4b390ebaa8cb219ac46a0608538827
Rushabh <rs@rushabhs.com>
2016-09-15 17:50:49 -0700

    station listing displayed

--- frontend/components/bart/stations.jsx
+++ frontend/components/bart/stations.jsx
@@ -3,12 +3,34 @@ import React from 'react';
 class AllStations extends React.Component {
   constructor(props){
     super(props);
-    debugger
+  }
+  componentDidMount(){
+    this.props.requestAllStations();
   }
   render() {
+    let content;
+    if (Object.keys(this.props.stations).length){
+      let allLis = Object.keys(this.props.stations).map(key => {
+        let currStation = this.props.stations[key];
+        return (
+          <li key={currStation.abbr["#text"]}>
+            {currStation.name["#text"]}
+          </li>
+        );
+      });
+      content = (
+        <ul>
+          {allLis}
+        </ul>
+      );
+    } else {
+      content = (
+        'Stations'
+      );
+    }
     return (
       <div>
-      Stations
+      {content}
       </div>
     );
   }
