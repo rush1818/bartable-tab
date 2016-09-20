2a2d74d268cf14729c63013a525c245e87ddbb3a
Rushabh <rs@rushabhs.com>
2016-09-20 11:39:22 -0700

    styling

--- frontend/components/routes/dropdown.jsx
+++ frontend/components/routes/dropdown.jsx
@@ -26,7 +26,7 @@ class DropDown extends React.Component {
     };
   }
   render() {
-    return (<div>
+    return (<div className='station-schedule'>
       <h1>View Station Schedule</h1>
       <StationsContainer type='View Station Info' handleChange={this.handleChange('selectedStation')} selectedStation={this.state.selectedStation} options={this.options} />
       <RouteInfoContainer abbr={this.state.selectedStation}/>
